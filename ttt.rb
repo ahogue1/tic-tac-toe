e84dc393bbce657cd88e3feb7cffe30e79066eb2
Amy Hogue <hogue.a@gmail.com>
2019-09-15 11:39:28 -0700

    Check for winning combinations method

--- ttt.rb
+++ ttt.rb
@@ -9,6 +9,7 @@ class TicTacToe
     @columns = ('A'..'C').to_a
 
     print_board
+    p is_winner?(@player)
   end
 
   def print_board
@@ -19,6 +20,24 @@ class TicTacToe
       puts "#{index + 1} | #{row.to_a.join(' | ')} |"
       puts '   -----------'
     end
+  end
+
+  def is_winner?(player)
+    winning_line = Array.new(3, player)
+
+    @board.row_vectors.each do |row|
+      return true if row.to_a == winning_line
+    end
+
+    @board.column_vectors.each do |col|
+      return true if col.to_a == winning_line
+    end
+
+    return true if @board.each(:diagonal).to_a == winning_line
+
+    mirrored_board = Matrix.rows(@board.row_vectors.map{ |r| r.to_a.reverse})
+
+    mirrored_board.each(:diagonal).to_a == winning_line
   end
 end
 
